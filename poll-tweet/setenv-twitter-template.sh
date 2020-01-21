ca040a0f51849ebd848c3ef8faccfa7f2f3c8222
Adam Boczek <adam.boczek@nttdata.com>
2020-01-21 13:07:21 +0100

    Fixed configuration errors for poll-tweet module

--- poll-tweet/setenv-twitter-template.sh
+++ poll-tweet/setenv-twitter-template.sh
@@ -7,3 +7,7 @@ export TWITTER_STATE_FILE=TwitterState.json
 export TWITTER_STATE_BUCKET=${SERVERLESS_STAGE}-sls-demo-twitter-state-${SERVERLESS_USER}
 export TWITTER_QUERY_TIMEOUT_SEC=10
 
+aws secretsmanager create-secret --name sls-demo-$SERVERLESS_USER/twitter/access-token --region $AWS_REGION --secret-string $TWITTER_ACCESS_TOKEN
+aws secretsmanager create-secret --name sls-demo-$SERVERLESS_USER/twitter/access-secret --region $AWS_REGION --secret-string $TWITTER_ACCESS_SECRET
+aws secretsmanager create-secret --name sls-demo-$SERVERLESS_USER/twitter/consumer-key --region $AWS_REGION --secret-string $TWITTER_CONSUMER_KEY
+aws secretsmanager create-secret --name sls-demo-$SERVERLESS_USER/twitter/consumer-secret --region $AWS_REGION --secret-string $TWITTER_CONSUMER_SECRET
