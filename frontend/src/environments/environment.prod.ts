b22a1be572ffd3cb58c5821a0a532f2853dfb62a
Adam Boczek <adam.boczek@nttdata.com>
2020-05-01 07:16:42 +0200

    Changed prod baseApiUrl to use dev backend

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApiUrl: "https://kccrwceyaj.execute-api.eu-central-1.amazonaws.com/prod"
+  baseApiUrl: "https://kccrwceyaj.execute-api.eu-central-1.amazonaws.com/dev"
 };
