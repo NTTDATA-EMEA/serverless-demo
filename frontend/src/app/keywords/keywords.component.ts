69ea75f696139f7a059ad25e06d69bebb67bef4d
Adam Boczek <adam.boczek@nttdata.com>
2020-05-01 09:28:34 +0200

    Added loading spinner to keywords component

--- frontend/src/app/keywords/keywords.component.ts
+++ frontend/src/app/keywords/keywords.component.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { environment } from "../../environments/environment";
+import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../../environments/environment";
 
 
 @Component({
@@ -18,20 +18,43 @@ export class KeywordsComponent implements OnInit {
 
   public selectedKeyword = '';
 
-  constructor(private readonly http: HttpClient) { }
+  public isLoading: boolean = true;
+
+  constructor(private readonly http: HttpClient) {
+  }
+
+  onLoad(): void {
+    this.isLoading = false;
+  }
 
   ngOnInit(): void {
-    this.http.get(this.keywordsUrl).subscribe(k => {
-      this.keywords = Object.keys(k);
+    this.http.get(this.keywordsUrl).subscribe({
+      next: k => {
+        this.keywords = Object.keys(k);
+      },
+      complete: () => {
+        this.isLoading = false;
+      }
     });
   }
 
   showBuzzwords(keyword: string) {
+    this.isLoading = true;
     this.selectedKeyword = keyword;
     this.buzzwords = [];
     keyword = keyword.startsWith('#') ? keyword.substring(1) : keyword;
-    this.http.get(this.buzzwordsUrl + '/' + keyword).subscribe((k: any) => {
-      this.buzzwords = Object.keys(k.buzzwords);
-    });
+    this.http.get(this.buzzwordsUrl + '/' + keyword)
+      .subscribe({
+        next: (k: any) => {
+          this.buzzwords = Object.keys(k.buzzwords);
+        },
+        error: () => {
+          this.buzzwords = [];
+          this.isLoading = false;
+        },
+        complete: () => {
+          this.isLoading = false;
+        }
+      });
   }
 }
