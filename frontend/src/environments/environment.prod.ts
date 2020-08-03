d5a42f7bfb53dea77666f8a3a6ecf7f66850d8eb
Adam Boczek <adam.boczek@nttdata.com>
2020-08-03 20:44:58 +0200

    Updated configuration for production

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseApiUrl: "https://lxbgb8f564.execute-api.eu-central-1.amazonaws.com/dev"
+  baseApiUrl: "https://4ujy00i859.execute-api.eu-central-1.amazonaws.com/prod"
 };
